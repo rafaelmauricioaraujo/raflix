196a8e5742a7131064f53b879f22d9c0c4caa842
Rafael Araujo <rafael.mauricio.araujo@gmail.com>
2020-07-27 22:21:59 -0300

    criando menu e botao da home

---
+++ src/components/Menu/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Logo from '../../assets/img/Logo.png';
+import './Menu.css';
+
+function Menu () {
+    return (
+        <nav className='Menu'>
+            
+            <a href='/'>
+                <img className='Logo' src={Logo} alt='Raflix Logo'></img>
+            </a>
+            
+            <a className='ButtonLink' href='/'>
+                Novo Video
+            </a>
+            
+        </nav>
+    );
+}
+
+export default Menu;
