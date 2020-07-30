0304b4a49801fb8cb73ec7026f2a75995c941939
Rafael Araujo <rafael.mauricio.araujo@gmail.com>
2020-07-29 22:01:19 -0300

    configurando mais páginas e componentes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path='/' component={Home} exact/>
+      <Route path='/cadastro/video' component={CadastroVideo} exact/>
+      <Route path='/cadastro/categoria' component={CadastroCategoria} exact/>
+      <Route component={Home} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
