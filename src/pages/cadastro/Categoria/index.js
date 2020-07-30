72ff414b9907bbd4da259fb20457d4b868d78d21
Rafael Araujo <rafael.mauricio.araujo@gmail.com>
2020-07-30 10:37:45 -0300

    criando mecanismo de cadastro no formulário

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
 
 function CadastroCategoria() {
+
+    const [nomeDaCategoria, setNomeDaCategoria] = useState('Filmes');
+    
     return (
         <PageDefault>
-            <h1>Cadastro de Categoria</h1>
+            <h1>Cadastro de Categoria: {nomeDaCategoria}</h1>
 
             <form>
 
@@ -13,6 +16,10 @@ function CadastroCategoria() {
                     Nome da Categoria:
                     <input
                         type="text"
+                        value={nomeDaCategoria}
+                        onChange={function handle(event){
+                            setNomeDaCategoria(event.target.value);
+                        }}
                     />
                 </label>
 
