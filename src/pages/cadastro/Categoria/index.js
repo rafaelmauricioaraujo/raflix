7914176085709e7e5410b8b39340564b9cf61556
Rafael Araujo <rafael.mauricio.araujo@gmail.com>
2020-07-30 10:49:14 -0300

    trabalhando no formulario

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -3,14 +3,20 @@ import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
 
 function CadastroCategoria() {
-
+    const [categorias, setCategorias] = useState(['Teste']);
     const [nomeDaCategoria, setNomeDaCategoria] = useState('Filmes');
-    
+
     return (
         <PageDefault>
             <h1>Cadastro de Categoria: {nomeDaCategoria}</h1>
 
-            <form>
+            <form onSubmit={function handleSubmit(event) {
+                event.preventDefault();
+                setCategorias([
+                    ...categorias,
+                    nomeDaCategoria
+                ]);
+            }}>
 
                 <label>
                     Nome da Categoria:
@@ -27,6 +33,16 @@ function CadastroCategoria() {
                     Cadastrar
                 </button>
             </form>
+
+            <ul>
+                {categorias.map((categoria, indice)=>{
+                    return(
+                        <li key={`${categoria}${indice}`}>
+                            {categoria}
+                        </li>
+                    )
+                })}
+            </ul>
 
             <Link to='/'>
                 Ir para Home
